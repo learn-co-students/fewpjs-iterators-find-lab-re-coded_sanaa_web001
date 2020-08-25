af54a1928d363770b1518bb61eb61e9a1a1c1b2d
Shihab AL-Mekhlafi <shihabalmekhlafy@gmail.com>
2020-08-25 23:25:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const testVar = {}
 
 function testFunc() {
-  return "hi"
+    return "hi"
+}
+
+let superbowlWin = (record) => {
+    let result = record.find(record => record.result === "W")
+   
+    if (!!result) {
+        return result.year;
+    } else {
+      
+        return  undefined;
+    }
 }
