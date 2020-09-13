8d0758e68cd43bbc624d4f54ea30dd18a162ce81
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-13 08:45:44 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(arrOfObj){
+   let winner=(arrOfObj.find(obj=>obj.result==="W"));
+   if(winner){return winner.year;}
+   else{return winner;}
+}
