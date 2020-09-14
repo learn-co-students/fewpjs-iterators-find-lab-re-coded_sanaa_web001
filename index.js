3a1155808bd4384f66de814781b765b8d35d514a
Asma Kolaib <as.kolaib@gmail.com>
2020-09-14 03:49:30 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let newArr = arr.find( i => i.result === "W" );
+  if (newArr === undefined ) {
+    return newArr ;
+  }
+  return newArr.year ;
+}
